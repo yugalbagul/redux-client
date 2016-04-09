f5312cf8c8f7b1529ee1331a68c3f8ba4b9d1c21
Yugal <yugalbagul2@gmail.com>
2016-04-09 17:05:29 +0530

    Initial Commit

---
+++ src/action_creators.js
@@ -0,0 +1,28 @@
+export function setState(state) {
+  return {
+    type: 'SET_STATE',
+    state
+  };
+}
+
+export function vote(entry) {
+  return {
+    meta: {remote: true},
+    type: 'VOTE',
+    entry
+  };
+}
+
+export function next() {
+  return {
+    meta: {remote: true},
+    type: 'NEXT'
+  };
+}
+
+export function reset() {
+  return {
+    meta: {remote: true},
+    type: 'RESET'
+  };
+}
